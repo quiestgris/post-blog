1122eaf1a71c9525371f5c100e91710f119eb2a6
quiestgris <sbaev72300@gmail.com>
2025-05-14 18:10:33 +0200

    modified

--- profil/scripts/logout.js
+++ profil/scripts/logout.js
@@ -1,4 +1,4 @@
 document.querySelector(".logout-btn").addEventListener("click", function() {
     localStorage.removeItem("user");
-    window.location.href = "../../index.html";
+    window.location.href = "../../";
 })
